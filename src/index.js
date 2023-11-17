02b34f583d24c1d40e363e3bc67fdb1655d62b31
Jennifer <jencegram@users.noreply.github.com>
2023-11-17 15:09:41 -0500

    Implement fruit selection and logging

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Importing the fruits array and the helper functions
+import fruits from './foods.js';
+import { choice, remove } from './helpers.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Randomly drawing a fruit from the array
+const randomFruit = choice(fruits);
+console.log(`I’d like one ${randomFruit}, please.`); 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(`Here you go: ${randomFruit}`); 
+console.log("Delicious! May I have another?"); 
+
+// Remove the fruit from the array of fruits
+remove(fruits, randomFruit);
+
+// Log the message about fruits left
+console.log(`I’m sorry, we’re all out. We have ${fruits.length} left.`); 
